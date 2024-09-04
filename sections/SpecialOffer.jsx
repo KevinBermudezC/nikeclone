1bb53344328f6da745ed4f46ce092f93550408c5
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-04 14:11:06 -0500

    LOL!

--- sections/SpecialOffer.jsx
+++ sections/SpecialOffer.jsx
@@ -1,6 +1,40 @@
+import {offer} from "@/public/assets/images";
+import Image from "next/image";
+import Button from "@/components/Button";
+import {arrowRight} from "@/public/assets/icons";
+
 const SpecialOffer = () => {
     return (
-        <h2>SpecialOffer</h2>
+        <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div className="flex-1 ">
+                <Image src={offer} alt={offer} width={773} height={687} className="object-contain w-full"/>
+            </div>
+            <div className='flex flex-1 flex-col'>
+                <h2 className='text-4xl font-palanquin font-bold'>
+                    <span className='text-coral-red'>Special </span>
+                    Offer
+                </h2>
+                <p className='mt-4 info-text'>
+                    Embark on a shopping journey that redefines your experience with
+                    unbeatable deals. From premier selections to incredible savings, we
+                    offer unparalleled value that sets us apart.
+                </p>
+                <p className='mt-6 info-text'>
+                    Navigate a realm of possibilities designed to fulfill your unique
+                    desires, surpassing the loftiest expectations. Your journey with us is
+                    nothing short of exceptional.
+                </p>
+                <div className='mt-11 flex flex-wrap gap-4'>
+                    <Button label='Shop now' iconURL={arrowRight}/>
+                    <Button
+                        label='Learn more'
+                        backgroundColor='bg-white'
+                        borderColor='border-slate-gray'
+                        textColor='text-slate-gray'
+                    />
+                </div>
+            </div>
+        </section>
     );
 };
 
