927bff2e07578b3065cdd98d86069668a70c2d02
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-03 20:50:06 -0500

    Insert Commit Message Here

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,27 @@
+import {headerLogo} from "../public/assets/images";
+import {hamburger} from "../public/assets/icons";
+import Image from "next/image";
+import {navLinks} from "@/constants";
 const Nav = () => {
     return (
-        <h2>Nav</h2>
+        <header className="padding-x py-8 absolute z-10 w-full">
+            <nav className="flex justify-between items-center max-container">
+                <a href="/">
+                    <Image priority src={headerLogo} alt="Logo" width={130} height={29}/>
+                </a>
+                <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                    {navLinks.map((item) => (
+                        <li key={item.label}>
+                            <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">{item.label}</a>
+                        </li>
+
+                    ))}
+                </ul>
+                <div className="hidden max-lg:block">
+                    <Image src={hamburger} alt="Hamburger" width={25} height={25}/>
+                </div>
+            </nav>
+        </header>
     );
 };
 
