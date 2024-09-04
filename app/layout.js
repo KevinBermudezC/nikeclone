d95a29e9cc148bf5d81362f2c427779447e94df6
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-03 20:25:12 -0500

    Gotta make you understand

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Nike Website",
+  title: "Nike",
   description: "Nike Website Clone with TailwindCSS and Next.js",
 };
 
