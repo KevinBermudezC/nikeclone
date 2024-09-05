0efe2b28e9d3759f691e1d18f1484e0ba23e96a2
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-05 13:31:35 -0500

    This is my code. My code is amazing.

---
+++ components/ReviewCard.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import {star} from "@/public/assets/icons";
+
+const ReviewCard = ({imgURL, customerName, rating, feedback}) => {
+    return (
+        <div className="flex justify-center items-center flex-col">
+            <Image src={imgURL} alt="customer" className="rounded-full object-cover w-[120px] h-[120px]"/>
+            <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+            <div className="mt-3 flex justify-center items-center gap-2.5">
+                <Image src={star} alt="rating" width={24} height={24} className="object-contain m-0" />
+                <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
+            </div>
+            <h3 className="mt-3 font-palanquin text-3xl font-bold text-center">
+                {customerName}
+            </h3>
+        </div>
+    );
+};
+
+export default ReviewCard;
--- sections/CustomerReviews.jsx
+++ sections/CustomerReviews.jsx
@@ -1,6 +1,22 @@
+import {reviews} from "@/constants";
+import ReviewCard from "@/components/ReviewCard";
+
 const CustomerReviews = () => {
     return (
-        <h2>CustomerReviews</h2>
+        <section className="max-container ">
+            <h3 className="font-palanquin text-center text-4xl font-bold">
+                What Our <span className="text-coral-red">Customers </span>Say?
+            </h3>
+            <p className="m-auto max-w-lg mt-4 text-center info-text">
+                Hear genuine stories from our satisfied customers about their
+                exceptional experiences with us.
+            </p>
+            <div className="flex flex-1 justify-evenly mt-24 items-center max-lg:flex-col gap-14">
+                {reviews.map((review) => (
+                    <ReviewCard key={review.customerName} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback}/>
+                ))}
+            </div>
+        </section>
     );
 };
 
