63ef3b7205c516e9eb32b420cc0b9153a865ae3c
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-05 14:21:17 -0500

    Fixed what was broken.

--- sections/Footer.jsx
+++ sections/Footer.jsx
@@ -1,6 +1,51 @@
+import Image from "next/image";
+import {footerLogo} from "@/public/assets/images";
+import {footerLinks, socialMedia} from "@/constants";
+import {copyrightSign} from "@/public/assets/icons";
+
 const Footer = () => {
     return (
-        <h2>Footer</h2>
+        <footer className="max-container">
+            <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+                <div className="flex flex-col items-start">
+                    <a href="#" className="text-3xl font-palanquin font-bold">
+                        <Image src={footerLogo} alt="logo" width={150} height={46}/>
+                    </a>
+                    <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
+                        Get shoes ready for the new term at your nearest Nike store. Find
+                        Your perfect Size In Store. Get Rewards
+                    </p>
+                    <div className="flex items-center gap-5 mt-8">
+                        {socialMedia.map((icon)=>(
+                            <div className="flex justify-center items-center w-12 h-12 rounded-full bg-white hover:bg-coral-red cursor-pointer">
+                                <Image src={icon.src} alt={icon.alt} width={24} height={24}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+                    {footerLinks.map((section)=>(
+                        <div key={section}>
+                            <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">{section.title}</h4>
+                            <ul>
+                                {section.links.map((link)=>(
+                                    <li key={link.name} className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer">
+                                        <a>{link.name}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+                <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+                    <Image src={copyrightSign} alt="copyright sign" width={20} height={20} className="rounded-full m-0"/>
+                    <p>Copyright. All Rights Reserved</p>
+                </div>
+                <p className="font-montserrat  cursor-pointer">Terms & Conditions</p>
+            </div>
+        </footer>
     );
 };
 
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
             <div
                 className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-cover bg-hero  bg-center'>
                 <Image src={bigShoeImg} alt="Shoe Collection" width={610} height={502}
-                       className="object-contain relative z-10"/>
+                       className="object-contain relative z-10" priority/>
                 <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
                     {shoes.map((image, index) => (
                         <div key={index}>
