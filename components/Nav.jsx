ab61658b5fff887c416f80e75b4c706bc0f276b6
KevinBermudezC <kevinbermudezc@gmail.com>
2024-09-05 15:15:28 -0500

    last minute fixes.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -27,7 +27,7 @@ const Nav = () => {
                     {navLinks.map((item) => (
                         <li key={item.label}>
                             <a href={item.href}
-                               className="font-montserrat leading-normal text-lg text-slate-gray">{item.label}</a>
+                               className="font-montserrat leading-normal text-lg text-slate-gray hover:text-coral-red ">{item.label}</a>
                         </li>
 
                     ))}
